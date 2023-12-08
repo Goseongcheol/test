7dc8352ca3f0cf2469f69874c290de19d5ec571e
Goseongcheol <tjdcjf0781@gmail.com>
2023-12-08 13:28:45 +0900

    sha

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { loginSuccessAction, logoutSuccessAction } from "./../../redux/action";
+import CryptoJS from "crypto-js";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -19,15 +20,19 @@ const App = () => {
   //   dispatch(logoutSuccessAction());
   // }, []);
 
-  const goLogin = async (id, password, dispatch) => {
+  const goLogin = async (id, password, dispatch, hashedPassword) => {
     try {
-      console.log(`아이디:${id}, password:${password}`);
+      console.log(
+        `아이디:${id}, password:${password},hashedPassword:${hashedPassword}`,
+      );
       await axios
-        .post(`https://meterarium.com:5280/login`, {
+        .post(`http://211.230.13.24:8080/logintest`, {
           id: id,
           password: password,
+          name: hashedPassword,
         })
         .then(async (res) => {
+          console.log(res);
           if (res.data.res !== null) {
             dispatch(loginSuccessAction(res.data.res)); // action dispatch
             const result = res.data.res.name;
@@ -38,6 +43,7 @@ const App = () => {
             console.log("로그인 성공");
             navigate("/map");
           } else {
+            console.log(res.data.res);
             alert("로그인 실패. 아이디와 비밀번호를 확인해주세요!");
             console.log("로그인 실패");
           }
@@ -53,7 +59,29 @@ const App = () => {
     } else if (!password) {
       alert("비밀번호를 입력해주세요");
     } else {
-      goLogin(id, password, dispatch);
+      const lastTwoDigits = id.slice(-2);
+
+      // "yit"과 id의 끝 2자리를 결합하여 새로운 솔트 생성
+      const userId = "yit" + lastTwoDigits;
+
+      function generateSaltBasedOnUserId(userId) {
+        // 사용자 아이디를 기반으로 SHA-256 해시를 생성하여 솔트로 사용
+        console.log("동작중");
+        console.log(CryptoJS.SHA256(userId).toString());
+        return CryptoJS.SHA256(userId).toString();
+      }
+
+      function hashPassword(password, salt) {
+        const hash = CryptoJS.SHA512(password + salt).toString();
+        console.log("비밀번호테스트");
+        console.log(hash);
+        console.log("비밀번호테스트2");
+        return hash;
+      }
+      // const userId = 'user123';
+      const salt = generateSaltBasedOnUserId(userId);
+      const hashedPassword = hashPassword(password, salt);
+      goLogin(id, password, dispatch, hashedPassword);
     }
   };
 
